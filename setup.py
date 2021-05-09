fa30faa288f7e364beb2db9b050b69415035bc4a
Terry Zheng <terryzhengtx@gmail.com>
2021-05-09 15:35:14 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 setup(
     name="fructose",
     packages=["fructose"],
-    version="1.0.0",
+    version="1.0.1",
     description= "Deployment CLI for synchronizing distributions with server.",
     long_description=long_description,
     long_description_content_type="text/markdown",
